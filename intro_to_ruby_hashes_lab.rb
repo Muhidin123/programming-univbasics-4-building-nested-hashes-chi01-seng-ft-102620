40aff1b40e3b3b462145e7976f9d2caa21f12436
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-17 01:51:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,9 +7,11 @@ end
 def monopoly_with_second_tier
   hash = {
     
-    :railroads => {pieces: 4, :rent_in_dollars => {}
+    :railroads => {pieces: 4, 
+    :rent_in_dollars => {},
+    :names => {}
       
-    },
+    }
   }
 end
 
